8465a100afda3dc2ec884f9ee32f1cdc19dd06f5
RAXON7013 <58189236+RAXON7013@users.noreply.github.com>
2019-12-11 17:26:06 +0100

    App version update

--- App.js
+++ App.js
@@ -1,40 +1,39 @@
-import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import Constants from 'expo-constants';
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import ListView from './components/ListView';
 
-// You can import from local files
-import AssetExample from './components/AssetExample';
+  /*
+   TodoApp: Haupteinstiegpunkt
+     render -> ListView wird angezeigt
+  */
 
-// or any pure javascript modules available in npm
-import { Card } from 'react-native-paper';
-
-export default class App extends React.Component {
+class TodoApp extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.paragraph}>
-          Change code in the editor and watch it change on your phone! Save to get a shareable url.
-        </Text>
-        <Card>
-          <AssetExample />
-        </Card>
+      Schulhaus Caguils Domat/Ems
+        <ListView></ListView>
       </View>
     );
   }
 }
 
+/*
+ Definition der CSS-Styles
+   im Objekt "styles" 
+*/
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-    padding: 8,
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
+    paddingTop: 30,
+    paddingBottom: 10,
+    paddingLeft: 2,
+    paddingRight: 2,
+    backgroundColor: '#F8F8F8',
+    fontSize: 20,
+    }
 });
+
+export default TodoApp
